69241a909d27ca0d4939d83f6b6d708255f5b0f0
AkhatorEnosa <aosakhogba@gmail.com>
2025-02-12 21:38:59 +0100

    Hero videos and onclick to change videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-center">
-      <h2 className="text-9xl font-bold underline">This is just the initialization</h2>
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+     <Hero />
+    </main>
   )
 }
 
